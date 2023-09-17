1944b66638760d9bcd61548c65c94ea29aff08db
cardevv <113392365+cardevv@users.noreply.github.com>
2023-09-17 20:18:52 -0300

    header input estilo

---
+++ space-app/src/componentes/CampoTexto/index.jsx
@@ -0,0 +1,56 @@
+import {styled} from 'styled-components'
+
+
+
+const ContainerEstilo = styled.div`
+position: relative;
+display: inline-block;
+
+      
+
+`
+
+const EstiloCampoTexto = styled.input`
+
+
+margin: 0px 20px;
+border-radius: 10px;
+border:  2px solid;
+border-color: #C98CF1;
+background:transparent;
+box-sizing: border-box;
+width: 602px;
+height: 50px;
+color: #D9D9D9;
+font-weight: 400;
+font-size: 20px;
+line-height: 20px;
+gap: 365;
+
+`
+
+
+const IconeLupa = styled.img`
+    position: absolute;
+    top: 5px;
+    right: 10px;
+    width: 38px;
+    height: 38px;
+`;
+
+
+const CampoTexto = () => {
+
+
+    return (
+
+        <ContainerEstilo>
+            <EstiloCampoTexto placeholder='O que você procura?'></EstiloCampoTexto>
+            <IconeLupa  src='./icones/search.png'  ></IconeLupa>
+
+        </ContainerEstilo>
+
+    )
+}
+
+export default CampoTexto
---
+++ space-app/src/componentes/EstiloGlobais/Cabecalho/index.jsx
@@ -0,0 +1,46 @@
+import {styled} from 'styled-components'
+import CampoTexto from '../../CampoTexto'
+
+
+
+const HeaderEstilizado = styled.header`
+
+padding: 50px 0;
+display: flex;
+justify-content: space-between;
+img {
+
+    margin: 0px 20px;
+    max-width: 212px;
+    
+}
+
+
+
+
+
+
+`
+
+
+
+const Cabecalho = () => {
+
+return(
+
+    <HeaderEstilizado>
+        <img   src='imagens/logo.png' alt=''></img>
+
+        <CampoTexto></CampoTexto>
+    </HeaderEstilizado>
+
+    
+   
+
+)
+  
+
+
+}
+
+export default Cabecalho
